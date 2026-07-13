a34a41210c2029f0794c8adf9c07c059e6906c80
Ismael Ramirez <ismaelramirez.dev@gmail.com>
2026-07-12 19:45:11 -0600

    Require password confirmation before project deletion

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,36 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import { Fragment } from 'react'
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { deleteProject, getProjects } from "@/api/ProjectAPI"
-import { toast } from "react-toastify"
+import { useQuery } from '@tanstack/react-query'
+import { getProjects } from "@/api/ProjectAPI"
 import { useAuth } from "@/hooks/useAuth"
 import { isManager } from "@/utils/policies"
+import DeleteProjectModal from "@/components/projects/DeleteProjectModal"
 
 export default function DashboardView() {
 
+  const location = useLocation()
+  const navigate = useNavigate()
   const { data: user, isLoading: authLoading } = useAuth()
-
   const { data, isLoading } = useQuery({
     queryKey: ['projects'],
     queryFn: getProjects
   })
-
-  const queryClient = useQueryClient()
-  const { mutate } = useMutation({
-    mutationFn: deleteProject,
-    onError: (error) => {
-      toast.error(error.message)
-    },
-    onSuccess: (data) => {
-      toast.success(data)
-      queryClient.invalidateQueries({ queryKey: ['projects'] })
-    }
-  })
-
-  console.log(data)
-
   if (isLoading && authLoading) return 'Loading..'
   if (data && user) return (
     <>
@@ -98,7 +84,7 @@ export default function DashboardView() {
                             <button
                               type='button'
                               className='block px-3 py-1 text-sm leading-6 text-red-500'
-                              onClick={() => mutate(project._id)}
+                              onClick={() => navigate(location.pathname + `?deleteProject=${project._id}`)}
                             >
                               Delete Project
                             </button>
@@ -119,9 +105,9 @@ export default function DashboardView() {
             className="text-fuchsia-500 font-bold"
           >Creat Project</Link>
         </p>
-
       )}
 
+      <DeleteProjectModal/>
     </>
 
   )
